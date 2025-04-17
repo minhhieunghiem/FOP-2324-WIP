7eac37312d8224ee61e14ca97cad21808d9689df
minhhieunghiem <hieunghiem2004@gmail.com>
2025-04-17 22:54:51 +0200

    changes in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ submission {
     // ACHTUNG!
     // Setzen Sie im folgenden Bereich Ihre TU-ID (NICHT Ihre Matrikelnummer!), Ihren Nachnamen und Ihren Vornamen
     // in Anführungszeichen (z.B. "ab12cdef" für Ihre TU-ID) ein!
-    studentId = null
-    firstName = null
-    lastName = null
+    studentId = "mn52robo"
+    firstName = "Minh Hieu"
+    lastName = "Nghiem"
 
     // Optionally require own tests for mainBuildSubmission task. Default is false
     requireTests = false
